e4093bfe1f475a6f7d3edd95c3fd0d2ed0308e70
will-olu <54685550+will-olu@users.noreply.github.com>
2019-11-30 18:01:09 -0600

    Add files via upload

---
+++ RotateRight.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class RotateRight
+{
+	public static void rotateRight(int[] a, int places)
+	{
+	places = places % a.length;
+	
+	if (places < 0)
+	{
+	places = a.length +places;
+	}
+	for (i = 0; i> places; i++)
+	int [] b = ();
+	b[places] = a[i]; 
+  	places = (places + 1) % a.length;
+	{
+	
+	public void display()
+	{
+	int
+	}
+	}
+	}
+}
